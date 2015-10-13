b999d24101cd2648aa479e0c62726c9d147b9e13
Alex Mathews <amathew9@jhu.edu>
2015-10-13 11:57:02 -0400

    got routing in individual js

--- BetterHierarchy/ngApp.js
+++ BetterHierarchy/ngApp.js
@@ -1,19 +1,10 @@
 // create the module and name it htmlDemo
-var ngApp = angular.module('ngApp', ['ngRoute', 'ngApp.homeView', 'ngApp.aboutView']);
+var ngApp = angular.module('ngApp', 
+	['ngRoute', 
+	 'ngApp.homeView', 
+	 'ngApp.aboutView']);
 
 // configure our routes
 ngApp.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-
-    // route for the home page
-    .when('/', {
-      templateUrl : 'views/home/home-view.html',
-      controller  : 'homeViewController'
-    })
-
-    //route for about page
-    .when('/about', {
-      templateUrl : 'views/about/about-view.html',
-      controller  : 'aboutViewController'
-    });
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
--- BetterHierarchy/views/about/aboutView.js
+++ BetterHierarchy/views/about/aboutView.js
@@ -1,13 +1,14 @@
-var aboutView = angular.module('ngApp.aboutView', []);
+var aboutView = angular.module('ngApp.aboutView', ['ngRoute']);
 
-// aboutView.config(['$routeProvider', function($routeProvider) {
-//   $routeProvider
-//     // route for the about page
-//     .when('/about', {
-//       templateUrl : 'views/about/about-view.html',
-//       controller  : 'aboutViewController'
-//     });
-// }]);
+aboutView.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  	//route to about
+    .when('/about', {
+      templateUrl : 'views/about/about-view.html',
+      controller  : 'aboutViewController'
+    });
+}]);
+
 
 aboutView.controller('aboutViewController', function($scope) {
   // create a message to display in our view
